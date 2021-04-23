d8249f1c92d035f9a4636a673584e074aaaa2352
Oliver Voogd <olivervoogd@gmail.com>
2021-04-23 14:45:20 +1000

    vignette changes for minimap2

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,7 +1,7 @@
 #' @importFrom basilisk BasiliskEnvironment
 flames_env <- BasiliskEnvironment(envname="flames_env", pkgname="FLAMES",
     packages=c("python==2.7.15.0",
-        "minimap2==2.17", 
+        #"minimap2==2.17", 
         "numpy==1.16.5", 
         "editdistance==0.5.3"),
     channels=c("bioconda", "conda-forge"),
