3f2888a5eefa42e9ac9b7b24c7706ca2c4579582
jakob-schuster <64141819+jakob-schuster@users.noreply.github.com>
2021-08-12 18:01:14 +1000

    added filter_TSS_TES

--- src/sc_longread/Isoforms.hpp
+++ src/sc_longread/Isoforms.hpp
@@ -5,6 +5,9 @@
 #include <numeric>
 #include "junctions.hpp"
 
+#include <iostream>
+#include <fstream>
+
 class Isoforms
 {
   private:
@@ -38,7 +41,7 @@ class Isoforms
     new_isoforms;
     std::map<std::string, int> 
     known_isoforms;
-    std::map<std::string, int> 
+    std::map<std::vector<int>, std::vector<std::pair<int, int>>> 
     raw_isoforms;
     std::map<std::string, int> 
     ge_dict;
@@ -54,7 +57,7 @@ class Isoforms
     std::pair<std::vector<int>, std::map<int, int>>
     group_sites(std::vector<int> l, int smooth_window, int min_threshold);
 
-    void filter_TSS_TES(std::string out_f, std::map<int, int>known_site, float fdr_cutoff);
+    void filter_TSS_TES(std::ofstream out_f, Junctions known_site, float fdr_cutoff);
 };
 
 Isoforms::Isoforms(std::string ch, std::map<std::string, int> config)
@@ -95,7 +98,7 @@ void Isoforms::add_isoform(Junctions junctions, bool is_reversed)
       // there is already stuff in single_block_dict
       // first, check if this key is already in there
       std::pair<int, int> target_key = 
-      {junctions.left, junctions.right};
+      {junctions.left[0], junctions.right[0]};
 
       if (this->single_block_dict.count(target_key) > 0)
       {
@@ -206,7 +209,7 @@ void Isoforms::add_one(Junctions junctions, bool strand)
   if (junctions.junctions.size() == 0) // single-exon
   {
     std::pair<int, int> key = 
-    {junctions.left, junctions.right};
+    {junctions.left[0], junctions.right[0]};
 
     this->single_block_dict[key] = this->single_blocks.size();
     this->single_blocks.push_back({key});
@@ -220,8 +223,8 @@ void Isoforms::add_one(Junctions junctions, bool strand)
   {
     this->junction_dict[junctions.junctions] = this->junction_list.size();
     this->junction_list.push_back({junctions.junctions});
-    this->left.push_back(junctions.left);
-    this->right.push_back(junctions.right);
+    this->left.push_back(junctions.left[0]);
+    this->right.push_back(junctions.right[0]);
     this->lr_pair[junctions.junctions] = {};
     
     this->strand_cnt[junctions.junctions] = {};
@@ -236,7 +239,7 @@ void Isoforms::update_one(Junctions junctions, std::vector<int> key, bool strand
   if (junctions.junctions.size()==0) // single-exon reads
   {
     std::pair<int, int> new_element = 
-    {junctions.left, junctions.right};
+    {junctions.left[0], junctions.right[0]};
 
     // we need to express the key as a pair here,
     // because we're looking through single_block_dict which is full of pairs
@@ -249,9 +252,9 @@ void Isoforms::update_one(Junctions junctions, std::vector<int> key, bool strand
   else // multi-exon reads
   {
     this->junction_list[this->junction_dict[key]].push_back(junctions.junctions);
-    this->left.push_back(junctions.left);
-    this->right.push_back(junctions.right);
-    this->lr_pair[key].push_back(std::pair<int, int> {junctions.left, junctions.right});
+    this->left.push_back(junctions.left[0]);
+    this->right.push_back(junctions.right[0]);
+    this->lr_pair[key].push_back(std::pair<int, int> {junctions.left[0], junctions.right[0]});
   }
   int multiplier = 1;
   if (this->STRAND_SPECIFIC) {multiplier = -1;}  
@@ -356,7 +359,7 @@ void Isoforms::update_all_splice()
   this->lr_pair = lr_pair_tmp;
 }
 
-void Isoforms::filter_TSS_TES(std::string out_f, std::map<int, int>known_site={}, float fdr_cutoff=0.01)
+void Isoforms::filter_TSS_TES(std::ofstream out_f, Junctions known_site={}, float fdr_cutoff=0.01)
 {
   std::string bedgraph_fmt = "{_ch}\t{_st}\t{_en}\t{_sc}\n";
 
@@ -408,8 +411,15 @@ void Isoforms::filter_TSS_TES(std::string out_f, std::map<int, int>known_site={}
       prob.emplace_back(rate * exp(-rate * i.second));
     }
     
+    // create and populate a cumulative_probability variable
     std::vector<int>
-    cumulative_probability = std::partial_sum(prob.begin(), prob.end(), 0);
+    cumulative_probability;
+    int running_total;
+    for (const auto & it : prob)
+    {
+      running_total += it;
+      cumulative_probability.push_back(running_total);
+    }
 
     if (cumulative_probability[0] > fdr_cutoff)
     {
@@ -432,18 +442,376 @@ void Isoforms::filter_TSS_TES(std::string out_f, std::map<int, int>known_site={}
     }
   };
 
-  auto insert_dist = [] (std::vector<std::pair<int, int>> fs, std::vector<int> known_site) 
+  auto insert_dist = [this] (std::vector<int> fs, std::vector<int> known_site) 
   {
-    std::vector<std::pair<int, int>>
+    std::vector<int>
     tmp = {fs[0]};
 
     if (fs.size() > 1)
     {
-      for (auto it = fs.begin() + 1; it != fs.end(); it++)
+      for (int it = 1; it != fs.size(); it++)
+      {
+        int
+        clo_p = take_closest(tmp, fs[it]);
+
+        if (abs(clo_p - fs[it]) > this->MAX_TS_DIST / 2)
+        {
+          tmp.push_back(fs[it]);
+        }
+      }
+    }
+
+    // if known_site is not None
+    if (known_site.size() > 0)
+    {
+      for (auto s : known_site)
       {
         int
         clo_p = take_closest(tmp, s);
+        if (abs(clo_p - s) > this->MAX_TS_DIST)
+        {
+          tmp.push_back(s);
+        }
       }
     }
+
+    return tmp;
   };
+  
+  // make counts objects for both left and right
+  std::map<int, int>
+  left_counts;
+
+  for (const auto & i : this->left)
+  {
+    left_counts[i]++;
+  }
+
+  std::vector<int>
+  cnt_l;
+
+  std::map<int, int>
+  right_counts;
+
+  for (const auto & i : this->right)
+  {
+    right_counts[i]++;
+  }
+
+  std::vector<int>
+  cnt_r;
+
+  if ((left_counts.size() < this->MIN_SUP_CNT) || (right_counts.size() < this->MIN_SUP_CNT))
+  {
+    return;
+  }
+  else
+  {
+    // left
+    std::vector<std::pair<int, int>>
+    fs_l = filter_site(left_counts);
+
+    
+    if (fs_l.size() == 0)
+    {
+      cnt_l = {-99999999};
+    }
+    else
+    {
+      // print everything to out_f
+      for (const auto & it : fs_l)
+      {
+        out_f << this->ch << "\t" 
+              << it.first << "\t" 
+              << it.first + 1 << "\t" 
+              << it.second << "\n";
+      }
+
+      // make a vector to store the keys from fs_l
+      std::vector<int> 
+      fs_l_keys;
+      for (const auto & i : fs_l)
+      {
+        fs_l_keys.push_back(i.first);
+      }
+
+      cnt_l = insert_dist(fs_l_keys, known_site.left);
+      std::sort(cnt_l.cbegin(), cnt_l.cend());
+    }
+
+    // right
+    std::vector<std::pair<int, int>>
+    fs_r = filter_site(right_counts);
+
+    if (fs_r.size() == 0)
+    {
+      cnt_r = {-99999999};
+    }
+    else
+    {
+      // print everything to out_f
+      for (const auto & it : fs_r)
+      {
+        out_f << this->ch << "\t"
+              << it.first << "\t"
+              << it.first + 1 << "\t"
+              << it.second << "\n";
+      }
+
+      // make a vector to store the keys from fs_r
+      std::vector<int>
+      fs_r_keys;
+      for (const auto & i : fs_r)
+      {
+        fs_r_keys.push_back(i.first);
+      }
+
+      cnt_r = insert_dist(fs_r_keys, known_site.right);
+      std::sort(cnt_r.cbegin(), cnt_r.cend());
+    }
+  }
+
+  for (const auto & pair : lr_pair)
+  {
+    // first we need a counts map, which we populate
+    std::map<std::pair<int, int>, int>
+    pair_counts;
+    for (const auto & i : pair.second)
+    {
+      pair_counts[i] ++;
+    }
+
+    // then to sort it, we need to convert to a vector
+    std::vector<std::pair<std::pair<int, int>, int>>
+    tmp_pair;
+    for (const auto & i : pair_counts)
+    {
+      tmp_pair.push_back(i);
+    }
+    std::sort(tmp_pair.cbegin(), tmp_pair.cend(),
+      [] (const auto & p1, const auto & p2)
+      {
+        return (p1.second > p2.second);
+      }
+    );
+
+    std::vector<std::pair<int, int>>
+    pair_after_filtering;
+
+    // lines like these make me think, 
+    // maybe i've not thought this architecture out well enough...
+    std::vector<std::pair<std::pair<int, int>, int>>
+    pair_enrich;
+
+    for (const auto & p : tmp_pair)
+    {
+      std::pair<int, int>
+      cl_p = {
+        take_closest(cnt_l, p.first.first),
+        take_closest(cnt_r, p.first.second)
+      };
+
+      if ((abs(cl_p.first - p.first.first) < (0.5 * this->MAX_TS_DIST)) and
+          (abs(cl_p.second - p.first.second) < 0.5 * this->MAX_TS_DIST))
+      {
+        if (pair_after_filtering.size() > 0)
+        {
+          // line 650 of sc_longread.py
+
+          // now we want to extract the left and right elements
+          std::vector<int>
+          pair_after_filtering_left;
+          std::vector<int>
+          pair_after_filtering_right;
+
+          for (const auto & it : pair_after_filtering)
+          {
+            pair_after_filtering_left.push_back(it.first);
+            pair_after_filtering_right.push_back(it.second);
+          }
+
+          int distance = (
+            abs(take_closest(pair_after_filtering_left, cl_p.first) - cl_p.first) +
+            abs(take_closest(pair_after_filtering_right, cl_p.second) - cl_p.second)
+          );
+
+          if (distance > this->MAX_TS_DIST)
+          {
+            if ((cl_p.first < pair.first.front()) and cl_p.second > pair.first.back())
+            {
+              pair_after_filtering.push_back(cl_p);
+            }
+          }
+        }
+        else
+        {
+          if ((cl_p.first < pair.first.front()) and (cl_p.second > pair.first.back()))
+          {
+            pair_after_filtering.push_back(cl_p);
+          }
+        }
+
+        // but we only want to allow up to MAX_SITE_PER_SLICE combinations
+        if (pair_after_filtering.size() >= this->MAX_SITE_PER_SLICE)
+        {
+          break;
+        }
+      }
+    }
+
+    if (pair_after_filtering.size() == 0)
+    {
+      // then search for isoform-specific enrichment
+      for (const auto & p : tmp_pair)
+      {
+        // we need to sum up all of the elements in tmp_pair that meet our criteria
+        int sum = 0;
+        for (const auto & it : tmp_pair)
+        {
+          if ((abs(it.first.first - p.first.first) + abs(it.first.second - p.first.second)) < this->MAX_TS_DIST)
+          {
+            sum += it.second;
+          }
+        }
+        pair_enrich.push_back({p.first, sum});
+      }
+      std::sort(
+        pair_enrich.cbegin(), pair_enrich.cend(),
+        [] (const auto & p1, const auto & p2)
+        {
+          // sign is flipped because we are reverse sorting
+          return (p1.second < p2.second);
+        }
+      );
+
+      for (const auto & p : pair_enrich)
+      {
+        if (pair_after_filtering.size() > 0)
+        {
+          // now we want to extract the left and right elements again
+          std::vector<int>
+          pair_after_filtering_left;
+          std::vector<int>
+          pair_after_filtering_right;
+
+          for (const auto & it : pair_after_filtering)
+          {
+            pair_after_filtering_left.push_back(it.first);
+            pair_after_filtering_right.push_back(it.second);
+          }
+
+          int distance = (
+            abs(take_closest(pair_after_filtering_left, p.first.first) - p.first.first) +
+            abs(take_closest(pair_after_filtering_right, p.first.second) - p.first.second)
+          );
+
+          if ((distance > this->MAX_TS_DIST) and 
+              (p.first.first < pair.first.front()) and 
+              (p.first.second > pair.first.back()))
+          {
+            // append the key of p
+            pair_after_filtering.push_back(p.first);
+          }
+        }
+        else if ((p.first.first < pair.first.front()) and (p.first.second > pair.first.back()))
+        {
+          // append the key of p
+          pair_after_filtering.push_back(p.first);
+        }
+
+        // we only want up to MAX_SITE_PER_SLICE combinations
+        if (pair_after_filtering.size() >= this->MAX_SITE_PER_SLICE)
+        {
+          // so just disreagard any after that point
+          break;
+        }
+      }
+    }
+
+    int sup_cnt_total = 0;
+
+    for (const auto & p : pair_after_filtering)
+    {
+      // now we want to add the values of keys matching certain criteria
+      for (const auto & it : tmp_pair)
+      {
+        if ((abs(it.first.first - p.first) + abs(it.first.second - p.second)) < this->MAX_TS_DIST)
+        {
+          sup_cnt_total += it.second;
+        }
+      }
+    }
+
+    if ((float(sup_cnt_total) / this->lr_pair.size()) <= this->MIN_SUP_PCT)
+    {
+      // not enough support counts - this often happens in the case when the TSS/TES is strongly degraded
+
+      if (pair_enrich.size() == 0)
+      {
+        for (const auto & p : tmp_pair)
+        {
+          // add it to pair_enrich
+          // we need to sum up all of the elements in tmp_pair that meet our criteria
+          int sum = 0;
+          for (const auto & it : tmp_pair)
+          {
+            if ((abs(it.first.first - p.first.first) + abs(it.first.second - p.first.second)) < this->MAX_TS_DIST)
+            {
+              sum += it.second;
+            }
+          }
+          pair_enrich.push_back({p.first, sum});
+        }
+        std::sort(
+          pair_enrich.cbegin(), pair_enrich.cend(),
+          [] (const auto & p1, const auto & p2)
+          {
+            // sign is flipped because we are reverse sorting
+            return (p1.second < p2.second);
+          }
+        );
+      }
+
+      std::vector<int>
+      tmp_ex;
+      tmp_ex.push_back(int(pair_enrich[0].first.first));
+      for (int i : pair.first)
+      {
+        tmp_ex.push_back(i);
+      }
+      tmp_ex.push_back(int(pair_enrich[0].first.second));
+
+      this->raw_isoforms[tmp_ex] = pair.second;
+      this->strand_cnt[tmp_ex] = this->strand_cnt[pair.first];
+    }
+    else
+    {
+      // now, add filtered TSS/TES to raw_isoforms
+      for (const auto & p : pair_after_filtering)
+      {
+        std::vector<int>
+        tmp_ex;
+        tmp_ex.push_back(int(p.first));
+        for (int i : pair.first)
+        {
+          tmp_ex.push_back(i);
+        }
+        tmp_ex.push_back(int(p.second));
+
+        // we need to sum up all of the elements in tmp_pair that meet our criteria
+        int sum = 0;
+        for (const auto & it : tmp_pair)
+        {
+          if ((abs(it.first.first - p.first) + abs(it.first.second - p.second)) < this->MAX_TS_DIST)
+          {
+            sum += it.second;
+          }
+        }
+
+        // come back and change this line later
+        this->raw_isoforms[tmp_ex] = {{sum, 0}};
+        this->strand_cnt[tmp_ex] = this->strand_cnt[pair.first];
+      }
+    }
+  }
 }
