84f52cea78518631ab112d3efe794b134d751e66
jakob <jakobschuster0@gmail.com>
2021-09-28 13:34:28 +1000

    changed find_isoform.cpp, removed junk

--- src/find_isoform.cpp
+++ src/find_isoform.cpp
@@ -1,7 +1,8 @@
 #include "find_isoform.h"
 
+// [[Rcpp::export]]
 void
-find_isoform
+find_isoform_cpp
 (
     std::string gff3, 
     std::string genome_bam, 
@@ -19,14 +20,23 @@ find_isoform
     // first, extract from the gff file
     Rcpp::List gene_anno = parse_gff_tree(gff3.c_str());
     
+    // convert the chr_to_gene Rcpp::List to a std::unordered_map
     std::unordered_map<std::string, std::vector<std::string>>
     chr_to_gene;
+    for (const auto & [key, val] : gene_anno["chr_to_gene"]) {
+        chr_to_gene[std::as(key)] = Rcpp::as(val);
+    }
+    
     std::unordered_map<std::string, Pos>
     transcript_dict;
+    
+    
     std::unordered_map<std::string, std::vector<std::string>>
     gene_to_transcript;
     std::unordered_map<std::string, std::vector<std::pair<int, int>>>
     transcript_to_exon;
+
+    
 
     
 }
--- src/find_isoform.h
+++ src/find_isoform.h
@@ -8,4 +8,4 @@
 #include "parse_gff_tree.h"
 
 void
-find_isoform(std::string gff3, std::string genome_bam, std::string isoform_gff3, std::string tss_test_stat, std::string genomefa, std::string transcript_fa, std::string downsample_ratio, Rcpp::List config_list, std::string raw_splice_isoform);
+find_isoform_cpp(std::string gff3, std::string genome_bam, std::string isoform_gff3, std::string tss_test_stat, std::string genomefa, std::string transcript_fa, std::string downsample_ratio, Rcpp::List config_list, std::string raw_splice_isoform);
