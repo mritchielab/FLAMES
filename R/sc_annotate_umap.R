8dd1f92a9f2f79ea53e2c75414fec8df7abfe095
Changqing Wang <wang.ch@wehi.edu.au>
2021-12-16 12:16:07 +1100

    update sc_annotate: better coloring for n_isoform=2

--- R/sc_annotate_umap.R
+++ R/sc_annotate_umap.R
@@ -26,7 +26,7 @@
 #' 
 #' @return a list containing the combined UMAP, the isoform exon alignments and the UMAP with isoform expression levels.
 #' 
-#' @importFrom dplyr group_by summarise_at top_n
+#' @importFrom dplyr group_by summarise_at slice_max filter
 #' @importFrom tidyr gather pivot_wider
 #' @importFrom magrittr "%>%"
 #' @importFrom S4Vectors DataFrame
@@ -35,7 +35,7 @@
 #' @importFrom scuttle addPerCellQC addPerFeatureQC isOutlier logNormCounts
 #' @importFrom scran getTopHVGs fixedPCA buildSNNGraph modelGeneVar
 #' @importFrom scater runUMAP
-#' @importFrom ggplot2 ggplot geom_point aes labs element_blank element_line theme_bw theme scale_colour_gradient2
+#' @importFrom ggplot2 ggplot geom_point aes labs element_blank element_line theme_bw theme scale_colour_gradient2 margin
 #' @importFrom ggbio autoplot
 #' @importFrom GenomicRanges GRanges GRangesList
 #' @importFrom rtracklayer import.gff3
@@ -43,8 +43,9 @@
 #' @importFrom igraph as_adjacency_matrix
 #' @importFrom rtracklayer import
 #' @importFrom Matrix t colSums
+#' @importFrom cowplot plot_grid get_legend
 #' @export
-sc_annotate_umap <- function(sce_20, sce_80, path, gene, n_isoforms = 4, dup_bc=NULL, sce_all = NULL, return_sce_all = T){
+sc_annotate_umap <- function(sce_20, sce_80, path, gene, n_isoforms = 4, n_pcs = 20, dup_bc=NULL, sce_all = NULL, return_sce_all = T){
   ## dup_bc: cell barcodes found in both lib_20 and lib_80
   ## the pipeline should filter out these cells (not implemented yet)
   ## this parameter will need to be removed after implementing filtering in the pipeline
@@ -72,7 +73,7 @@ sc_annotate_umap <- function(sce_20, sce_80, path, gene, n_isoforms = 4, dup_bc=
     cat("Running PCA for sce_all ...\n")
     sce_all <- logNormCounts(sce_all)
     hvgs <- getTopHVGs(modelGeneVar(sce_all), n=2000)
-    sce_all <- fixedPCA(sce_all, rank=20, subset.row=hvgs)
+    sce_all <- fixedPCA(sce_all, rank=n_pcs, subset.row=hvgs)
   }
 
   if ( !("UMAP" %in% reducedDimNames(sce_all)) ) {
@@ -91,7 +92,7 @@ sc_annotate_umap <- function(sce_20, sce_80, path, gene, n_isoforms = 4, dup_bc=
     cat("running PCA for sce_20 ...\n")
     sce_20 <- logNormCounts(sce_20)
     hvgs <- getTopHVGs(modelGeneVar(sce_20), n=2000)
-    sce_20 <- fixedPCA(sce_20, rank=20, subset.row=hvgs)
+    sce_20 <- fixedPCA(sce_20, rank=n_pcs, subset.row=hvgs)
   }
   snn <- buildSNNGraph(sce_20, use.dimred = "PCA")
   snn_mat <- as_adjacency_matrix(snn, attr="weight")
@@ -102,11 +103,6 @@ sc_annotate_umap <- function(sce_20, sce_80, path, gene, n_isoforms = 4, dup_bc=
   transcript_count <- read.csv(file.path(path,"transcript_count.csv.gz"), stringsAsFactors=FALSE)
   isoform_FSM_annotation <- read.csv(file.path(path,"isoform_FSM_annotation.csv") , stringsAsFactors=FALSE)
   isoform_gff <- rtracklayer::import.gff3(file.path(path,"isoform_annotated.filtered.gff3"))
-
-  # Debug
-  #cat("isoform_gff: ")
-  #cat(class(isoform_gff))
-  #cat("\n")
  
   transcript_count <- transcript_count[match(isoform_FSM_annotation$transcript_id,transcript_count$transcript_id),]
   transcript_count$FSM_match <- isoform_FSM_annotation$FSM_match
@@ -137,6 +133,7 @@ sc_annotate_umap <- function(sce_20, sce_80, path, gene, n_isoforms = 4, dup_bc=
   keep.hi <- isOutlier(tr_sce$sum, type="higher", log=TRUE)
   keep.low <- isOutlier(tr_sce$sum, type="lower", log=TRUE)
   tr_sce <- tr_sce[,(!keep.hi) & (!keep.low)]
+  tr_sce <- logNormCounts(tr_sce)
  
   rowData(tr_sce)$gene_id <- gsub("\\..*","", rowData(tr_sce)$gene_id)
   #gene_name = mapIds(org.Hs.eg.db,
@@ -152,31 +149,36 @@ sc_annotate_umap <- function(sce_20, sce_80, path, gene, n_isoforms = 4, dup_bc=
   # row_meta columns: "transcript_id", "gene_id", "FSM_match", "mean", "detected", "gene_name"
   # rows: transcript_id
   row_meta <- as.data.frame(rowData(tr_sce_multi))
-  row_meta <- row_meta %>% group_by(gene_id) %>% top_n(n = n_isoforms, wt = mean) #keep top n isoforms per gene
- 
+  row_meta <- row_meta %>% dplyr::filter(gene_id == gene) %>% slice_max(n = n_isoforms, order_by = mean) #keep top n isoforms per gene
+  if (dim(row_meta)[1] < 2) {
+    stop(paste0("Alternative isoform not found for the given gene: ", gene))
+  } 
   tr_sce_multi <- tr_sce_multi[rowData(tr_sce_multi)$transcript_id %in% row_meta$transcript_id,] # apply top n to sce
-  tr_sce_multi <- logNormCounts(tr_sce_multi)
+  tr_na <- row_meta$FSM_match  
   
   isoform_gff$Parent <- as.character(isoform_gff$Parent)
   isoform_gff$transcript_id <- unlist(lapply(strsplit(isoform_gff$Parent, split = ":"),function(x){x[2]}))
-  sel_tr <- rowData(tr_sce_multi)[rowData(tr_sce_multi)$gene_id==gene,"transcript_id"]
-  sel_fsm <- rowData(tr_sce_multi)[rowData(tr_sce_multi)$gene_id==gene,"FSM_match"]
-  isoform_sel <- isoform_gff[isoform_gff$transcript_id %in% sel_tr,]
-  isoform_sel <- split(isoform_sel, isoform_sel$transcript_id) # ???
-  names(isoform_sel) <- sel_fsm[match(names(isoform_sel),sel_tr)]
-  # Debug
-  ## (?) why isoform_sel is not GRangeList object already?
-  #cat("isoform_sel: ")
-  #cat(class(isoform_sel))
-  #cat("\n")
-  isoform_sel <- GRangesList(isoform_sel)
-  plot_isoforms <- ggbio::autoplot(isoform_sel, label = TRUE) + 
-    theme_bw()+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-                    panel.background = element_blank(), axis.line = element_line(colour = "black"))
+  sel_tr <- rowData(tr_sce_multi)[,"transcript_id"]
+  sel_fsm <- rowData(tr_sce_multi)[,"FSM_match"]
+
+  isoform_sel <- isoform_gff[isoform_gff$transcript_id %in% sel_tr,] 
+  isoform_sel <- S4Vectors::split(isoform_sel, isoform_sel$transcript_id) # Split isoforms into a list
+  names(isoform_sel) <- sel_fsm[match(names(isoform_sel),sel_tr)] #Set names to FSM
+  isoform_sel <- isoform_sel[tr_na] # Set order
+  if (length(isoform_sel) == 2) {
+    fill_by_isoform <- c( rep("#A50026", length(isoform_sel[[1]])), rep("#313695", length(isoform_sel[[2]])) )
+    plot_isoforms <- ggbio::autoplot(isoform_sel, label = TRUE, fill = fill_by_isoform) + 
+      theme_bw()+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                       panel.background = element_blank(), axis.line = element_line(colour = "black"))
+  } else {
+    plot_isoforms <- ggbio::autoplot(isoform_sel, label = TRUE) + 
+      theme_bw()+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                       panel.background = element_blank(), axis.line = element_line(colour = "black"))
+    # Autoplot does not follow the order in isoform_sel ...
+  }
  
   ### impute transcript counts for cells in lib_20
   cat("Imputing transcript counts for cells in lib_20 ...\n")
-  tr_na <- rownames(rowData(tr_sce_multi))[rowData(tr_sce_multi)$gene_id==gene] #Transcript_ids of the gene
   expr <- logcounts(tr_sce_multi)[tr_na,]
   expr_20 <- matrix(0,nrow(expr),ncol(sce_20))  #dim: transcripts x cells, initialised to 0
   rownames(expr_20) <- rownames(expr)
@@ -190,6 +192,7 @@ sc_annotate_umap <- function(sce_20, sce_80, path, gene, n_isoforms = 4, dup_bc=
   
   expr_20 <- t(t(expr_20)/colSums(snn_mat))
   expr_20 <-  scale(expr_20)
+  ## TODO: fix scale when n_isoforms == 2
 
   reduce_quantile <- function(x, q=0.05) {
     x[x<quantile(x, q, na.rm=T)] <- quantile(x, q, na.rm=T)
@@ -208,32 +211,71 @@ sc_annotate_umap <- function(sce_20, sce_80, path, gene, n_isoforms = 4, dup_bc=
   umap_20 <- as.data.frame(sce_all@int_colData$reducedDims$UMAP)
   umap_20 <- umap_20[sce_all$lib == "lib20",]
   colnames(umap_20) <- c("x", "y") 
-  umap_20 <- cbind(t(expr_20), umap_20)
 
   if (n_isoforms > length(tr_na)) {
     n_isoforms <- length(tr_na)
   }
-  plot_idx <- function(idx) {
-    p <- ggplot()+
+
+  if (n_isoforms == 2) {
+    umap_20 <- cbind(expr=expr_20[names(isoform_sel)[1],], umap_20)
+    plot_expression_umaps <- ggplot()+
       geom_point(data = umap_80, aes(x=x,y=y),alpha=0.2,size=0.2, col='grey', show.legend = F)+
-      geom_point(data = umap_20, aes(x=x,y=y,col=umap_20[,idx]),size=0.7)+
-      labs(x="Dim1",y="Dim2",col="scaled expression",title = colnames(umap_20)[idx])+
-      scale_colour_gradient2(low = "#313695", mid = "#FFFFBF", high = "#A50026", na.value = NA, midpoint=median(umap_20[,idx]))+
+      geom_point(data = umap_20, aes(x=x, y=y, col=expr), size=0.7) +
+      labs(x="Dim1",y="Dim2",col="scaled expression")+
+      scale_colour_gradient2(low = "#313695", mid = "#FFFFBF", high = "#A50026", na.value = NA, midpoint=0)+
       theme_bw()+
-      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-            panel.background = element_blank(), panel.border = element_blank(),axis.line = element_line(colour = "black"),
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(), 
+            panel.border = element_blank(),
+            axis.line = element_line(colour = "black"),
             axis.text.x=element_blank(),
             axis.ticks.x=element_blank(),
             axis.text.y=element_blank(),
-            axis.ticks.y=element_blank())
-    p
+            axis.ticks.y=element_blank(),
+            legend.text = element_blank())
+    combined_isoform_plot <- plot_grid(plot_isoforms@ggplot,plot_expression_umaps, ncol = 1, rel_heights = c(1, 2.5))
+  } else {
+    umap_20 <- cbind(t(expr_20), umap_20)
+    # ggplot2 lazy evaluation with for loops
+    #   may need to unquote idx with !! operator
+    plot_idx <- function(idx) {
+      p <- ggplot()+
+        geom_point(data = umap_80, aes(x=x,y=y),alpha=0.2,size=0.2, col='grey', show.legend = F)+
+        geom_point(data = umap_20, aes(x=x,y=y,col=umap_20[,idx]),size=0.7)+
+        labs(x="Dim1",y="Dim2",col="scaled expression",title = colnames(umap_20)[idx])+
+        scale_colour_gradient2(low = "#313695", mid = "#FFFFBF", high = "#A50026", na.value = NA, midpoint=median(umap_20[,idx]))+
+        theme_bw()+
+        theme(panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.background = element_blank(), 
+              panel.border = element_blank(),
+              axis.line = element_line(colour = "black"),
+              axis.text.x=element_blank(),
+              axis.ticks.x=element_blank(),
+              axis.text.y=element_blank(),
+              axis.ticks.y=element_blank(),
+              legend.position="none")
+      p
+    }
+    plot_expression_umaps <- lapply(1:n_isoforms, plot_idx)
+  
+    legend <- ggplot(data.frame(x=1:2,y=1:2)) + 
+      geom_point(aes(x=x,y=y, col=c(-1,1))) + 
+      labs(col="scaled expression") +
+      scale_colour_gradient2(low = "#313695", mid = "#FFFFBF", high = "#A50026",
+                             breaks = c(-0.8, 0.8),
+                             labels=c("Low","High"))
+    legend <- get_legend(legend + theme(legend.box.margin = margin(0, 0, 0, 12)))
+    combined_isoform_plot <- plot_grid(plotlist =  c(list(annotation=plot_isoforms@ggplot),plot_expression_umaps)) 
+    combined_isoform_plot <- plot_grid(combined_isoform_plot, legend, rel_widths = c(3, .4)) 
   }
-  plot_expression_umaps <- lapply(1:n_isoforms, plot_idx)
+
 
   outputs <- list(plot_umap = plot_umap,
                 plot_isoforms = plot_isoforms, 
                 plot_expression_umaps = plot_expression_umaps,
-                combined_isoform_plot = do.call(plot_grid, c(list(annotation=plot_isoforms@ggplot),plot_expression_umaps))) 
+                combined_isoform_plot = combined_isoform_plot) 
   
   if (return_sce_all) {
     outputs[["sce_all"]] <- sce_all
