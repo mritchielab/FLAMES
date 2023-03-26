56b866e3519e5722129af5a48ae74c21a4b556a5
Changqing Wang <wang.ch@wehi.edu.au>
2023-03-26 16:29:31 +1100

    wip: agrep_demultiplex

--- R/agrep_demultiplex.R
+++ R/agrep_demultiplex.R
@@ -1,20 +1,25 @@
 # work in progress
 library("ShortRead")
 library("Biostrings")
-agrep_demultiplex_ <- function(fastq_file, bc.list, out.file = "out.fq.gz", structure = list(adaptor.p5 = "CTACAC",
-  barcode = ".{16}", adaptor.read1nspacer = "CGCGTC.{28}GAGACAG", seq = ".*"),
-  trim.after = "CTGTCTCTTATACACATCTCCGAGCCCACGAGAC", strands = c("-", "+"), pattern.max.distance = 3,
-  bc.max.distance = 2) {
-
+agrep_demultiplex_ <- function(
+    fastq_file, bc.list, out.file = "out.fq.gz", structure = list(
+      adaptor.p5 = "CTACAC",
+      barcode = ".{16}", adaptor.read1nspacer = "CGCGTC.{28}GAGACAG", seq = ".*"
+    ),
+    trim.after = "CTGTCTCTTATACACATCTCCGAGCCCACGAGAC", strands = c("-", "+"), pattern.max.distance = 3,
+    bc.max.distance = 2) {
   cat(fastq_file, "\n")
-  strm <- FastqStreamer(fastq_file)
-  nreads <- 0
-  while (length(res <- yield(strm))) {
-    nreads <- nreads + length(res)
+  stat.names <- c("matched", "found.2N", "bc", "bc.dist")
 
+  strm <- FastqStreamer(fastq_file)
+  while (nreads <- length(res <- yield(strm))) {
     match.idx <- sapply(strands, function(x) {
-      NULL
-    })
+      data.frame(matrix(ncol = length(stat.names), nrow = nreads, dimnames = list(
+        NULL,
+        stat.names
+      )))
+    }, simplify = FALSE)
+
     for (strand in sort(strands, decreasing = TRUE)) {
       if (strand == "-") {
         res <- reverseComplement(res)
@@ -22,11 +27,11 @@ agrep_demultiplex_ <- function(fastq_file, bc.list, out.file = "out.fq.gz", stru
 
       pattern <- paste0(c("(", paste0(structure, collapse = ")("), ")"), collapse = "")
       m <- aregexec(pattern, sread(res), max.distance = pattern.max.distance)
-      match.idx[[strand]] <- sapply(m, function(x) {
+      match.idx[[strand]]$matched <- sapply(m, function(x) {
         x[1] != -1
       })
-      res.matched <- res[match.idx[[strand]]]
-      m <- m[match.idx[[strand]]]
+      res.matched <- res[match.idx[[strand]]$matched]
+      m <- m[match.idx[[strand]]$matched]
       if (!length(m)) {
         next
       }
@@ -35,14 +40,15 @@ agrep_demultiplex_ <- function(fastq_file, bc.list, out.file = "out.fq.gz", stru
         bc <- sapply(regmatches(sread(res.matched), m), function(x) {
           x[which("barcode" == names(structure)) + 1]
         })
-          bc.exact <- bc %in% bc.list
+        bc.exact <- bc %in% bc.list
         if (bc.max.distance >= 1 && length(bc[!bc.exact])) {
           bc.dists <- sapply(bc[!bc.exact], function(x) {
-          adist(x, bc.list)
+            adist(x, bc.list)
           })
           bc.fuzzy <- apply(bc.dists, 2, function(x) {
-          ifelse(min(x) < bc.max.distance && sum(x == min(x)) == 1, which.min(x),
-            -1)
+            ifelse(min(x) < bc.max.distance && sum(x == min(x)) == 1, which.min(x),
+              -1
+            )
           })
           bc[!bc.exact][bc.fuzzy != -1] <- bc.list[bc.fuzzy[bc.fuzzy != -1]]
 
@@ -52,20 +58,23 @@ agrep_demultiplex_ <- function(fastq_file, bc.list, out.file = "out.fq.gz", stru
           bc.matched <- bc.exact
         }
         match.idx[[strand]][match.idx[[strand]]][!bc.matched] <- FALSE
-        readseq <- DNAStringSet(sapply(regmatches(sread(res.matched), m),
+        readseq <- DNAStringSet(sapply(
+          regmatches(sread(res.matched), m),
           function(x) {
-          x[which("seq" == names(structure)) + 1]
-          }))[bc.matched]
+            x[which("seq" == names(structure)) + 1]
+          }
+        ))[bc.matched]
         readid <- BStringSet(paste(bc, id(res.matched), sep = "#"))[bc.matched]
         readqual <- sapply(regmatches(quality(res.matched), m), function(x) {
           x[which("seq" == names(structure)) + 1]
         })[bc.matched]
-
       } else {
-        readseq <- DNAStringSet(sapply(regmatches(sread(res.matched), m),
+        readseq <- DNAStringSet(sapply(
+          regmatches(sread(res.matched), m),
           function(x) {
-          x[which("seq" == names(structure)) + 1]
-          }))
+            x[which("seq" == names(structure)) + 1]
+          }
+        ))
         readid <- id(res.matched)
         readqual <- sapply(regmatches(quality(res.matched), m), function(x) {
           x[which("seq" == names(structure)) + 1]
@@ -73,18 +82,25 @@ agrep_demultiplex_ <- function(fastq_file, bc.list, out.file = "out.fq.gz", stru
       }
 
       if (!missing(trim.after) && !is.null(trim.after)) {
-        alig <- pairwiseAlignment(subject = trim.after, pattern = readseq,
-          type = "overlap")
+        alig <- pairwiseAlignment(
+          subject = trim.after, pattern = readseq,
+          type = "overlap"
+        )
         readseq <- subseq(readseq, start = ifelse(score(alig) > 10, end(pattern(alig)),
-          1))
+          1
+        ))
         readqual <- SFastqQuality(subseq(readqual, start = ifelse(score(alig) >
           10, end(pattern(alig)), 1)))
       }
 
-      demultiplexed_res <- new("ShortReadQ", sread = readseq, quality = readqual,
-        id = readid)
-      writeFastq(object = demultiplexed_res[width(demultiplexed_res) > 10],
-        mode = "a", file = out.file)
+      demultiplexed_res <- new("ShortReadQ",
+        sread = readseq, quality = readqual,
+        id = readid
+      )
+      writeFastq(
+        object = demultiplexed_res[width(demultiplexed_res) > 10],
+        mode = "a", file = out.file
+      )
     }
     if (length(strands) > 1) {
       cat(as.character(id(res[match.idx[["+"]] & match.idx[["-"]]])), sep = "\n")
@@ -93,7 +109,6 @@ agrep_demultiplex_ <- function(fastq_file, bc.list, out.file = "out.fq.gz", stru
   }
   close(strm)
 
-  stats <- data.frame(nreads = nreads)
   if (length(strands) > 1) {
     stats["n.mixed.strands"] <- sum(match.idx[["+"]] & match.idx[["-"]])
   }
@@ -103,45 +118,38 @@ agrep_demultiplex_ <- function(fastq_file, bc.list, out.file = "out.fq.gz", stru
   return(stats)
 }
 
-agrep_demultiplex <- function(fastq_file, bc.list, out.file = "out.fq.gz", structure = list(adaptor.p5 = "CTACAC",
-  barcode = ".{16}", adaptor.read1nspacer = "CGCGTC.{28}GAGACAG", seq = ".*"),
-  trim.after = "CTGTCTCTTATACACATCTCCGAGCCCACGAGAC", strands = c("-", "+"), pattern.max.distance = 3,
-  bc.max.distance = 2) {
+agrep_demultiplex <- function(
+    fastq_file, bc.list, out.file = "out.fq.gz", structure = list(
+      adaptor.p5 = "CTACAC",
+      barcode = ".{16}", adaptor.read1nspacer = "CGCGTC.{28}GAGACAG", seq = ".*"
+    ),
+    trim.after = "CTGTCTCTTATACACATCTCCGAGCCCACGAGAC", strands = c("-", "+"), pattern.max.distance = 3,
+    bc.max.distance = 2) {
   if (length(fastq_file) == 1) {
     return(agrep_demultiplex_(fastq_file, bc.list, out.file, structure, trim.after,
-      strands, pattern.max.distance, bc.max.distance = 2))
+      strands, pattern.max.distance,
+      bc.max.distance = 2
+    ))
   } else {
     bcs <- data.frame()
     for (i in fastq_file) {
       bcs <- rbind(bcs, agrep_demultiplex_(i, bc.list, out.file, structure,
-        trim.after, strands, pattern.max.distance, bc.max.distance = 2))
+        trim.after, strands, pattern.max.distance,
+        bc.max.distance = 2
+      ))
     }
     return(bcs)
   }
 }
 
-barcodes.atac.tsv <- read.csv("../barcodes.atac.tsv", header = F)$V1
-fq_files <- c("../pass/fastq_pass/PAM86628_pass_f70417de_14b6a3ab_1378.fastq.gz",
-  "../pass/fastq_pass/PAM86628_pass_f70417de_14b6a3ab_221.fastq.gz", "../pass/fastq_pass/PAM86628_pass_f70417de_14b6a3ab_978.fastq.gz",
-  "../pass/fastq_pass/PAM86628_pass_f70417de_14b6a3ab_311.fastq.gz", "../pass/fastq_pass/PAM86628_pass_f70417de_14b6a3ab_616.fastq.gz",
-  "../pass/fastq_pass/PAM86628_pass_f70417de_14b6a3ab_23.fastq.gz", "../pass/fastq_pass/PAM86628_pass_f70417de_14b6a3ab_1117.fastq.gz",
-  "../pass/fastq_pass/PAM86628_pass_f70417de_14b6a3ab_340.fastq.gz", "../pass/fastq_pass/PAM86628_pass_f70417de_14b6a3ab_498.fastq.gz",
-  "../pass/fastq_pass/PAM86628_pass_f70417de_14b6a3ab_713.fastq.gz")
+if (FALSE) {
+  barcodes.atac.tsv <- read.csv("./barcodes.atac.tsv", header = F)$V1
+  cat(format(Sys.time(), "%a %b %d %X"), "\n")
+  x <- agrep_demultiplex(list.files(c("../fastq_pass", "../fastq_fail"),
+    full.names = TRUE,
+    pattern = "*.fastq.gz"
+  ), barcodes.atac.tsv, bc.max.distance = 1, pattern.max.distance = 3)
+  cat(format(Sys.time(), "%a %b %d %X"), "\n")
 
-cat(format(Sys.time(), "%a %b %d %X"), "\n")
-x <- agrep_demultiplex(fq_files, barcodes.atac.tsv, bc.max.distance = 1, pattern.max.distance = 3)
-cat(format(Sys.time(), "%a %b %d %X"), "\n")
-
-saveRDS(x, "stats.rds")
-
-
-# structure <- list(`-` = list(seq = '.*', adaptor.read1nspacer =
-# 'CTGTCTC.{28}GACGCG', barcode = '.{16}', adaptor.p5 = 'GTGTAG'), `+` =
-# list(adaptor.p5 = 'CTACAC', barcode = '.{16}', adaptor.read1nspacer =
-# 'CGCGTC.{28}GAGACAG', seq = '.*'))
-
-#'((\\[.+\\])|([A-Za-z\\.]))(\\{[0-9,]+\\})'
-# complement_regex <- function(str) { new_str = '' for (i in strsplit(str,
-# split = '')) { new_str <- paste0(c(new_str, switch(i, A = 'T', C = 'G', T =
-# 'A', G = 'C', i))) return(new_str) } } reverse_pattern <- '(' for
-# (regex_group in rev(structure)) { }
+  saveRDS(x, "stats.rds")
+}
