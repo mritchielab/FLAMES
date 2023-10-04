dfa2ba7bb797f2930dbccc8f9a7ad3cf20e5a036
youyupei <youyupei@gmail.com>
2023-10-04 16:38:04 +1100

    using matplotlib from pip to avoid the libz error

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,7 +1,7 @@
 #' @importFrom basilisk BasiliskEnvironment
 flames_env <- BasiliskEnvironment(
     envname = "flames_env", pkgname = "FLAMES",
-    pip = c("fast-edit-distance==1.2.1", "blaze2==2.0.0a9"),
+    pip = c("fast-edit-distance==1.2.1", "blaze2==2.0.0a9", "matplotlib==3.5.3"),
     packages = c(
         "python==3.10",
         "numpy==1.25.0",
@@ -9,7 +9,6 @@ flames_env <- BasiliskEnvironment(
         "pysam==0.21.0",
         "cutadapt==4.4",
         "tqdm==4.64.1",
-        "matplotlib==3.5.3",
         "pandas==1.3.5"
     ),
     channels = c("conda-forge", "bioconda", "defaults")
